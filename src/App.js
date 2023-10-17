b3b0b275fa1b0b0be7a6053011f42c106a62ede0
harshugpt <uharshgpt2912@gmail.com>
2023-10-18 00:11:29 +0530

    added custom data and created custom wordlehook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Wordle } from "./components/Wordle";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    /*fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });*/
+    axios.get("http://localhost:3001/solutions").then((res) => {
+      const response = res.data;
+      const randomSolution =
+        response[Math.floor(Math.random() * response.length)];
+      setSolution(randomSolution.word);
+    });
+  }, [setSolution]);
   return (
     <div className="App">
       <h1>Wordle Clone</h1>
+      {solution && <Wordle solution={solution} />}
     </div>
   );
 }
---
+++ src/hooks/useWordle.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const useWordle = ({ solution }) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([]);
+  const [history, setHistory] = useState([]);
+  const [isCorrect, setIsCorrect] = useState(false);
+
+  const formatGuess = () => {};
+  const addNewGuess = () => {};
+  const handleKeyup = (e) => {
+    console.log(e);
+  };
+
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+};
+
+export default useWordle;
