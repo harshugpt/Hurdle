c6cd6435c0f02eb7f5b7ab425cc77fa8eb8f155f
harshugpt <uharshgpt2912@gmail.com>
2023-10-19 23:56:06 +0530

    added condition for coloured letters and displaying live text

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Row from "./Row";
 
-const Grid = ({ currentguess, turn, guesses }) => {
+const Grid = ({ currentGuess, turn, guesses }) => {
   return (
     <div>
       {guesses.map((g, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />;
+        }
         return <Row key={i} guess={g} />;
       })}
     </div>
